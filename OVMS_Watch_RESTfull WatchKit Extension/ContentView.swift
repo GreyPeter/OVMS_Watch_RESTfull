1d5d7fe7449b87d63dec23615ed31ce9d8ea6951
Peter Harry <p.harry@me.com>
2022-07-21 14:08:36 +1000

    Initial Commit

---
+++ OVMS_Watch_RESTfull WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OVMS_Watch_RESTfull WatchKit Extension
+//
+//  Created by Peter Harry on 21/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OVMS_Watch_RESTfull WatchKit Extension/OVMS_Watch_RESTfullApp.swift
@@ -0,0 +1,19 @@
+//
+//  OVMS_Watch_RESTfullApp.swift
+//  OVMS_Watch_RESTfull WatchKit Extension
+//
+//  Created by Peter Harry on 21/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct OVMS_Watch_RESTfullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
